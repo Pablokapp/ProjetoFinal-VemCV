694bab88ed2291035c0e3b4bfe6e68bb95ed3b38
Gustavo Barbosa <gbdsantos0@inf.ufpel.edu.br>
2022-05-01 21:55:03 -0300

    feat: bloqueado o cadastro de cadastradores

--- src/main/java/com/dbc/vemcv/security/WebSecurityConfig.java
+++ src/main/java/com/dbc/vemcv/security/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable().and().cors().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/auth", "/user/create-cadastrador").permitAll()
+                .antMatchers("/", "/auth"/*, "/user/create-cadastrador"*/).permitAll()
                 .antMatchers("/**").hasRole("CADASTRADOR")
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new TokenAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
